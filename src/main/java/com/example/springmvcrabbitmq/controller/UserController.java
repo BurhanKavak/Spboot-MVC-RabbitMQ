3d636f3534b3f792a5fcad13c83240214ad07d3c
Burhan <burhan.kvk58@gmail.com>
2023-06-02 01:04:58 +0300

    update

--- src/main/java/com/example/springmvcrabbitmq/controller/UserController.java
+++ src/main/java/com/example/springmvcrabbitmq/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.example.springmvcrabbitmq.controller;
 
+import com.example.springmvcrabbitmq.dto.response.UserDtoForResponse;
 import com.example.springmvcrabbitmq.model.User;
+import com.example.springmvcrabbitmq.model.messages.ApiResponse;
 import com.example.springmvcrabbitmq.repository.UserRepository;
 import com.example.springmvcrabbitmq.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/user")
@@ -14,4 +18,13 @@ public class UserController {
     private final UserService userService;
 
 
+    @GetMapping
+    public ApiResponse<List<UserDtoForResponse>> getUsers() {
+        return userService.getUsers();
+    }
+
+    @PostMapping
+    public ApiResponse<UserDtoForResponse> createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
 }
--- src/main/java/com/example/springmvcrabbitmq/model/messages/ResponseMessage.java
+++ src/main/java/com/example/springmvcrabbitmq/model/messages/ResponseMessage.java
@@ -6,6 +6,6 @@ import java.util.Date;
 public class ResponseMessage {
 
     public static Timestamp timestamp = new Timestamp(new Date().getTime());
-    public static String success = "Operation is OK.";
+    public static String success = "Operation is success.";
     public static String fail = "Operation is fail.";
 }
